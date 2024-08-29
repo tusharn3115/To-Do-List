73b869030234dad2bb322b8c1db2c651fea8afe5
Tushar Negi <negitushar81@gmail.com>
2024-08-29 19:20:53 +0530

    added some js logic for task selection and unselection

---
+++ script.js
@@ -0,0 +1,27 @@
+const checkbox = document.querySelectorAll(".circle");
+const inputList = document.querySelectorAll(".goal-input");
+const warning = document.querySelector(".warning");
+const progressBar = document.querySelector(".progress-bar");
+const progressCompleted = document.querySelector(".progress-completed");
+
+checkbox.forEach((checkbox) => {
+    checkbox.addEventListener('click', (e) => {
+        const allfieldsfilled = [...inputList].every(function(input){
+            return input.value;
+        })
+        if(allfieldsfilled){
+            checkbox.parentElement.classList.toggle('completed');
+            progressCompleted.style.paddingLeft = '15px'
+            progressCompleted.style.width = '33.33%'
+        }
+        else{
+            warning.style.display = "block";
+        }
+    })
+})
+
+inputList.forEach((input) => {
+    input.addEventListener('focus', () => {
+        warning.style.display = "none";
+    })
+})
